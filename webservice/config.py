b994fd4ee02db23beaa22916eb0a7b2069907ac4
Rafael Nogales <xrnogales@gmail.com>
2019-04-23 14:35:29 +0200

    Add new scraper instance

--- webservice/config.py
+++ webservice/config.py
@@ -5,4 +5,5 @@ SCRAPER_ENDPOINT = '/api/scrape'
 SCRAPERS = [
     'https://xbot-scraper01.herokuapp.com/',
     'https://xbot-scraper02.herokuapp.com/',
+    'https://xbot-scraper03.herokuapp.com/',
 ]
