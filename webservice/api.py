91d038aaa85a7e269e26c260f09f8bc4d90c0ea4
Rafael Nogales <xrnogales@gmail.com>
2019-04-23 22:37:03 +0200

    Log url and origin

--- webservice/api.py
+++ webservice/api.py
@@ -3,11 +3,16 @@ from flask_cors import CORS
 from webservice.config import SCRAPERS, SCRAPER_ENDPOINT
 import random
 import json
+import re
 from requests import get as get_url
 
 xbot_webservice = Flask(__name__)
 CORS(xbot_webservice)
 current_scraper = random.choice(SCRAPERS)
+
+def captureURLs(text):
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+    return urls
 
 
 @xbot_webservice.route("/")
@@ -34,6 +39,15 @@ def redirect_scrape():
 
 @xbot_webservice.route('/api/newoffer', methods=['POST'])
 def new_offer():
+    payload = request.json
+    message = payload.get('message')
+    origin = payload.get('origin')
+
+    urls = captureURLs(message)
+    for url in urls:
+        print(url)
+        print(origin)
+
     dummy = {
         "short_description": "Samsung EVO Plus - Tarjeta de Memoria microSD de 128 GB con Adaptador SD, 100 MB/s, U3, Color Rojo y Blanco",
         "description": "Tarjeta de memoria microSD Samsung EVO PLUS 128Gb MB-MC128GAEU + adaptador SD incluido en el blíster. 100Mb/s de lectura y 90 Mb/de escritura. Controladora U3.",
@@ -46,6 +60,7 @@ def new_offer():
         "size": None
     }
     return Response(json.dumps(dummy), status=200, mimetype='application/json')
+
 
 if __name__ == '__main__':
     xbot_webservice.run(debug=True)
