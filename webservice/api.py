776c81541093efc8a427d87a7a81c5b72522d292
Rafael Nogales <xrnogales@gmail.com>
2019-04-23 21:43:37 +0200

    Add dummy endpoint

--- webservice/api.py
+++ webservice/api.py
@@ -32,5 +32,20 @@ def redirect_scrape():
         return Response(json.dumps(r.json()), status=r.status_code, mimetype='application/json')
 
 
+@xbot_webservice.route('/api/newoffer', methods=['POST'])
+def new_offer():
+    dummy = {
+        "short_description": "Samsung EVO Plus - Tarjeta de Memoria microSD de 128 GB con Adaptador SD, 100 MB/s, U3, Color Rojo y Blanco",
+        "description": "Tarjeta de memoria microSD Samsung EVO PLUS 128Gb MB-MC128GAEU + adaptador SD incluido en el blíster. 100Mb/s de lectura y 90 Mb/de escritura. Controladora U3.",
+        "features": "Lectura 100 Mb/s\nEscritura 90 Mb/s\nControladora U3\n› Ver más detalles",
+        "standard_price": "EUR 88,99",
+        "end_date": None,
+        "price": "EUR 23,92",
+        "url": "https://www.amazon.es/gp/product/B06XFHQGB9?pf_rd_p=44ae21af-cb43-440e-bcab-82e6a5324e39",
+        "image_url": "https://images-na.ssl-images-amazon.com/images/I/61EA%2B6P9I0L._SL1150_.jpg",
+        "size": None
+    }
+    return Response(json.dumps(dummy), status=200, mimetype='application/json')
+
 if __name__ == '__main__':
     xbot_webservice.run(debug=True)
