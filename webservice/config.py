b9c47dd66d5af6ed70232c9f398b3321218eeb04
Rafael Nogales <xrnogales@gmail.com>
2019-04-23 21:08:11 +0200

    Add new scraper instances

--- webservice/config.py
+++ webservice/config.py
@@ -6,4 +6,11 @@ SCRAPERS = [
     'https://xbot-scraper01.herokuapp.com/',
     'https://xbot-scraper02.herokuapp.com/',
     'https://xbot-scraper03.herokuapp.com/',
+    'https://xbot-scraper04.herokuapp.com/',
+    'https://xbot-scraper05.herokuapp.com/',
+    'https://xbot-scraper06.herokuapp.com/',
+    'https://xbot-scraper07.herokuapp.com/',
+    'https://xbot-scraper08.herokuapp.com/',
+    'https://xbot-scraper09.herokuapp.com/',
+    'https://xbot-scraper10.herokuapp.com/',
 ]
