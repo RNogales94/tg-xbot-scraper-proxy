e7cfc8f61973f4ef1cb0a623d8cdcee2e5d7e3ec
Rafael Nogales <xrnogales@gmail.com>
2019-05-13 21:54:59 +0200

    Restart scraper when fail

--- webservice/api.py
+++ webservice/api.py
@@ -5,6 +5,8 @@ import random
 import json
 import re
 import requests
+import urllib
+import os
 
 from xbot.xbotdb import Xbotdb
 from xbot.product import loadProductfromJSON
@@ -37,20 +39,50 @@ def get_scraper(user):
         return current_scraper
 
 
+"""
+DELETE /apps/xbot-scraper01b/dynos/web.1 HTTP/1.1
+Host: api.heroku.com
+Content-Type: application/json
+Accept: application/vnd.heroku+json; version=3
+Authorization: Basic aW5mb0B4Ym90LmRldjowNjA4TmV5dW1hbnNhJA==
+cache-control: no-cache
+Postman-Token: 1b4c0af9-e7fd-4725-b14d-8af2112ae9a4
+"""
+
+
+def get_app_name(scraper_address):
+    return urllib.parse.urlparse(scraper_address).hostname.split('.')[0]
+
+
+def restart(scraper_address):
+    r = requests.delete(f'https://api.heroku.com/apps/{get_app_name(scraper_address)}/dynos/web.1', headers={
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.heroku+json; version=3',
+    }, auth=(os.environ['HEROKU_SCRAPERS_USER'], os.environ['HEROKU_SCRAPERS_PASS']))
+    print(f'Restarting {get_app_name(scraper_address)}')
+    print(f'{r.content}')
+    return r.status_code
+
+
+
+
+
 def update_current_scraper(user):
     global current_scraper
     global current_scraper_pro
     global current_api_scraper
 
     if user in pro_users:
-        current_scraper_pro = random.choice(list(set(SCRAPERS_PRO) - set([current_scraper_pro])))
+        restart(current_scraper_pro)
 
+        current_scraper_pro = random.choice(list(set(SCRAPERS_PRO) - set([current_scraper_pro])))
         # Avoid duplicated Dynos running
-        current_api_scraper = random.choice(list(set(SCRAPERS_XBOT) - set([current_api_scraper])))
+        current_api_scraper = current_scraper_pro
 
     elif user == 'XBOT_API':
+        restart(current_api_scraper)
         current_scraper_pro = random.choice(list(set(SCRAPERS_PRO) - set([current_scraper_pro])))
-        current_api_scraper = random.choice(list(set(SCRAPERS_XBOT) - set([current_api_scraper])))
+        current_api_scraper = current_scraper_pro
     else:
         current_scraper = random.choice(list(set(SCRAPERS) - set([current_scraper])))
 
