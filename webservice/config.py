63973a58aa7b44e9554b3d32d3fa5da3b00493ef
Rafael Nogales <xrnogales@gmail.com>
2019-05-30 19:20:53 +0200

    New scraper pool

--- webservice/config.py
+++ webservice/config.py
@@ -1,3 +1,5 @@
+import os
+
 # BASE URL FOR SCRAPERS
 
 SCRAPER_ENDPOINT = '/api/scrape'
@@ -11,14 +13,21 @@ SCRAPERS = [
     'https://xbot-scraper03.herokuapp.com/',
 ]
 
+code = ''
+
+if os.environ['HEROKU_SCRAPERS_USER'] == 'katerina.lopatkova95@gmail.com':
+    code = 'c'
+elif os.environ['HEROKU_SCRAPERS_USER'] == 'info@xbot.dev':
+    code = 'b'
+
 SCRAPERS_PRO = [
-    'https://xbot-scraper01b.herokuapp.com/',
-    'https://xbot-scraper02b.herokuapp.com/',
-    'https://xbot-scraper03b.herokuapp.com/',
+    f'https://xbot-scraper01{code}.herokuapp.com/',
+    f'https://xbot-scraper02{code}.herokuapp.com/',
+    f'https://xbot-scraper03{code}.herokuapp.com/',
 ]
 
 SCRAPERS_XBOT = [
-    'https://xbot-scraper01b.herokuapp.com/',
-    'https://xbot-scraper02b.herokuapp.com/',
-    'https://xbot-scraper03b.herokuapp.com/',
+    f'https://xbot-scraper01{code}.herokuapp.com/',
+    f'https://xbot-scraper02{code}.herokuapp.com/',
+    f'https://xbot-scraper03{code}.herokuapp.com/',
 ]
