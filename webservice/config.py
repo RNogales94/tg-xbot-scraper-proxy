673b3f37932d87d9ce769e00026bbfb3ed293bcd
Rafael Nogales <xrnogales@gmail.com>
2019-05-30 19:46:10 +0200

    Use only two nodes

--- webservice/config.py
+++ webservice/config.py
@@ -23,11 +23,22 @@ elif os.environ['HEROKU_SCRAPERS_USER'] == 'info@xbot.dev':
 SCRAPERS_PRO = [
     f'https://xbot-scraper01{code}.herokuapp.com/',
     f'https://xbot-scraper02{code}.herokuapp.com/',
-    f'https://xbot-scraper03{code}.herokuapp.com/',
+    # f'https://xbot-scraper03{code}.herokuapp.com/',
 ]
 
 SCRAPERS_XBOT = [
     f'https://xbot-scraper01{code}.herokuapp.com/',
     f'https://xbot-scraper02{code}.herokuapp.com/',
-    f'https://xbot-scraper03{code}.herokuapp.com/',
+    # f'https://xbot-scraper03{code}.herokuapp.com/',
 ]
+
+
+# CHROMEDRIVER_PATH     /app/.chromedriver/bin/chromedriver
+
+# GOOGLE_CHROME_BIN     /app/.apt/usr/bin/google-chrome
+
+# https://github.com/heroku/heroku-buildpack-google-chrome
+
+# https://github.com/heroku/heroku-buildpack-chromedriver
+
+# heroku/python
